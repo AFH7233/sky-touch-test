3f4b2b97d8a0130706c80a5255dc01c0d1e469f0
afuentes <andres7233@hotmail.com>
2018-09-10 17:29:52 -0500

    ManagementApplication is added, and it runs the mvc web app. Also folder /target is added to gitignore

---
+++ management-api/src/main/java/com/afh/skytouch/managment/configuration/ManagementApplication.java
@@ -0,0 +1,18 @@
+package com.afh.skytouch.managment.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.afh.skytouch")
+public class ManagementApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ManagementApplication.class);
+    }
+}
