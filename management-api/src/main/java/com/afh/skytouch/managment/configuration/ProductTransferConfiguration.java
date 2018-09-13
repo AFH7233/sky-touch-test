1d613c4831e53e9f42a035d59eb0238d11ce3cba
Andres Fuentes <andres7233@hotmail.com>
2018-09-13 16:40:04 -0500

    Fix for ProductTransferConfiguration. properties for module crud is added

--- management-api/src/main/java/com/afh/skytouch/managment/configuration/ProductTransferConfiguration.java
+++ management-api/src/main/java/com/afh/skytouch/managment/configuration/ProductTransferConfiguration.java
@@ -1,7 +1,12 @@
 package com.afh.skytouch.managment.configuration;
 
 
-import org.springframework.amqp.core.*;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
