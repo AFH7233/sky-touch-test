45997609e17390fdf70f9e50f614e9f2ae2df60c
Andres Fuentes <andres7233@hotmail.com>
2018-09-17 18:08:23 -0500

    Configuration to enable entity scan.

--- product-crud/src/main/java/com/afh/skytouch/management/configuration/ProductServerConfiguration.java
+++ product-crud/src/main/java/com/afh/skytouch/management/configuration/ProductServerConfiguration.java
@@ -1,11 +1,20 @@
 package com.afh.skytouch.management.configuration;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
 @SpringBootApplication
 @ComponentScan("com.afh.skytouch")
+@EntityScan(basePackages = {"com.afh.skytouch.commons"})
+@EnableJpaRepositories(basePackages = "com.afh.skytouch.management.repositories")
 public class ProductServerConfiguration {
     public static void main(String[] args){
         SpringApplication.run(ProductServerConfiguration.class);
--- product-crud/src/main/java/com/afh/skytouch/management/queue/listeners/ProductCreationListener.java
+++ product-crud/src/main/java/com/afh/skytouch/management/queue/listeners/ProductCreationListener.java
@@ -2,7 +2,6 @@ package com.afh.skytouch.management.queue.listeners;
 
 import com.afh.skytouch.commons.configuration.ProductTransferConfiguration;
 import com.afh.skytouch.commons.dto.GenericProduct;
-import com.afh.skytouch.management.repositories.interfaces.ProductRepository;
 import com.afh.skytouch.management.repositories.interfaces.ProductRepositoryCustom;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +12,6 @@ public class ProductCreationListener {
 
     @Autowired
     ProductRepositoryCustom repository;
-
-    @Autowired
-    ProductRepository repo;
 
     @RabbitListener(queues = ProductTransferConfiguration.create)
     public void onMessage(GenericProduct product){
