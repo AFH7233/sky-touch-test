f5875e211edb166dd008069dd5e61e4e7e29c1d9
Andres Fuentes <andres7233@hotmail.com>
2018-09-20 12:39:57 -0500

    Fixed not picking properties from common jar

--- product-crud/src/main/java/com/afh/skytouch/services/product/configuration/ProductServerConfiguration.java
+++ product-crud/src/main/java/com/afh/skytouch/services/product/configuration/ProductServerConfiguration.java
@@ -3,11 +3,13 @@ package com.afh.skytouch.services.product.configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.afh.skytouch")
 @EntityScan(basePackages = {"com.afh.skytouch.commons"})
 @EnableJpaRepositories(basePackages = "com.afh.skytouch.management.repositories")
+@PropertySource("classpath:common.properties")
 public class ProductServerConfiguration {
     public static void main(String[] args){
         SpringApplication.run(ProductServerConfiguration.class);
